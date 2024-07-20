8d921c8e337f06d839c0810d15bc145f214ed0cb
Mustafa <pourmhere@gmail.com>
2024-07-20 15:04:23 +0530

    code added

--- script.js
+++ script.js
@@ -8,20 +8,51 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  const devs = arr.map((e) => {
+    if (e.profession === "developer") {
+      return e;
+    }
+  }).filter(Boolean);
+
+  console.log(devs);
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  const devArr = [];
+  arr.forEach(e => {
+    if (e.profession === "developer") {
+      devArr.push(e);
+
+    }
+  });
+  console.log(devArr);
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newEmployee);
+
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  const filteredEmployees = arr.filter((val) => val.profession !== "admin");
+  console.log(filteredEmployees);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const newArr = [
+    { id: 5, name: "M", age: "22", profession: "demo" },
+    { id: 6, name: "K", age: "23", profession: "test" },
+    { id: 7, name: "J", age: "24", profession: "Rest" },
+  ];
+
+  const mix = arr.concat(newArr);
+  console.log(mix);
 }
+
+
